89509c23f680cc60b22cd2ae86369062bf1b77d1
camilotrejos <c.juantrejos@gmail.com>
2021-10-24 07:18:46 -0500

    Manejo de sesiones

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from sqlite3.dbapi2 import TimestampFromTicks
+from flask import Flask, render_template, request, redirect, make_response, session
 from flask.scaffold import _matching_loader_thinks_module_is_package
 from flask_wtf import FlaskForm, form, recaptcha
 from flask_wtf.recaptcha.fields import RecaptchaField
@@ -25,7 +26,7 @@ def sql_connection():
 class login_form(FlaskForm):
     username = StringField('Correo', validators=[InputRequired(message='El usuario es requerido')])
     password = PasswordField("Contraseña", validators=[InputRequired(message='Contraseña es requerida')])
-    recaptcha = RecaptchaField()
+    #recaptcha = RecaptchaField()
 
 class register_form(FlaskForm):
     Nusername = StringField('Nombre Usuario', validators=[InputRequired(message='El mombre de usuario es requerido')])
@@ -43,8 +44,18 @@ class add_comentario(FlaskForm):
     comentario = TextAreaField('Comentario', validators=[InputRequired(message='Escriba un comentario')])
     rating = SelectField('Calificación', choices=[(1,'1'), (2,'2'),(3,'3'), (4,'4'), (5,'5')])
 
+
+
 @app.route('/', methods=['GET'])
 def index():
+
+
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
 
 
     conP = sql_connection()
@@ -53,10 +64,18 @@ def index():
     curP.execute(statementP)
     dataP = curP.fetchall()
 
-    return render_template('index.html', page='inicio',  pelis = dataP)
+    return render_template('index.html', page='inicio',  pelis = dataP, user_tipo = user_tipo, user_id = user_id)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
+
     form = login_form()
     if form.validate_on_submit():
 
@@ -65,23 +84,43 @@ def login():
         statement = 'SELECT * FROM Usuarios WHERE correo = ?' 
         cur.execute(statement, [form.username.data])
         data = cur.fetchall()
-        
-        try:
 
+        try:
+            
             if data [0][2] == form.password.data:
+
+                
+                
                 if data[0][5] == 'Usuario':
+
+                    session['user_tipo'] = data[0][5]
+                    session['user_id'] = data[0][0]
+
                     return redirect('perfilUsuario/'+str(data[0][0]))
+
                 if data[0][5] == 'Admin':
+
+                    session['user_tipo'] = data[0][5]
+                    session['user_id'] = data[0][0]
+                    
                     return redirect('dashboard')
-        
+
         except:
             pass
             
-    return render_template('login.html', form=form, recaptcha=recaptcha, page='login')
+    return render_template('login.html', form=form, recaptcha=recaptcha, page='login', user_tipo=user_tipo, user_id=user_id)
         
  
 @app.route('/registrar', methods=['GET','POST'])
 def registrar():
+
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
+    
     register = register_form()
     if register.validate_on_submit():
                 
@@ -93,7 +132,7 @@ def registrar():
 
         return redirect (url_for('login'))
     else:
-        return render_template('registrar.html', page='registrar', form = register)
+        return render_template('registrar.html', page='registrar', form = register, user_tipo=user_tipo, user_id=user_id)
 
 
 
@@ -101,7 +140,13 @@ def registrar():
 @app.route('/dashboard/', methods=['GET','POST'])
 def dashboard():
 
-    
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
+
     #Consulta de PELICULAS
 
     conP = sql_connection()
@@ -118,10 +163,21 @@ def dashboard():
     curU.execute(statementU)
     dataU = curU.fetchall()
 
-    return render_template('dashboard.html', page='dashboard', user = dataU, pelis = dataP)
+    if user_tipo=="Admin":
+        return render_template('dashboard.html', page='dashboard', user = dataU, pelis = dataP, user_tipo=user_tipo, user_id=user_id)
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route('/detalleDeLaFuncion/<id>', methods=['GET','POST'])
 def detalle_de_funcion(id):
+
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
 
     conP = sql_connection()
     curP = conP.cursor()
@@ -129,14 +185,29 @@ def detalle_de_funcion(id):
     curP.execute(statementP, [id])
     dataP = curP.fetchall()
 
-    try:
-        return render_template('detalleDeLaFuncion.html', page='detalleFuncion', pelis = dataP[0])
 
-    except:
-        return redirect(url_for('index'))
+    if user_tipo != "Invitado":
+        try:
+            return render_template('detalleDeLaFuncion.html', page='detalleFuncion', pelis = dataP[0], user_tipo=user_tipo, user_id=user_id)
+
+        except:
+            return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+    
         
 @app.route('/detalleDePelicula/<id>', methods=['GET'])
 def detalle_pelicula(id):
+
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
+
+    
 
     #COnsulta Pelicula
     conP = sql_connection()
@@ -154,7 +225,7 @@ def detalle_pelicula(id):
     dataC = curC.fetchall()
 
     try:
-        return render_template('detalleDePelicula.html', page='detallePelicula', pelis = dataP[0], com = dataC)
+        return render_template('detalleDePelicula.html', page='detallePelicula', pelis = dataP[0], com = dataC, user_tipo=user_tipo, user_id=user_id)
 
     except:
         return redirect(url_for('index'))
@@ -162,16 +233,34 @@ def detalle_pelicula(id):
 
 @app.route('/perfilUsuario/<nom_usuario>', methods=['GET'])
 def perfil_usuario(nom_usuario):
-    return render_template('perfilUsuario.html', page='perfilUsuario', name = nom_usuario)
 
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
 
-@app.route('/perfilUsuario/', methods=['GET'])
-def perfil_user():
-    return render_template('perfilUsuario.html', page='perfilUsuario', name = "invitado")
+    if user_tipo == "Usuario":
+        if str(nom_usuario) == str(user_id):
+            return render_template('perfilUsuario.html', page='perfilUsuario', name = nom_usuario, user_tipo=user_tipo, user_id=user_id)
+        else:
+            return redirect('../perfilUsuario/'+str(user_id))
+    elif user_tipo == "Admin":
+        return redirect(url_for('dashboard'))
 
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/peliculas/', methods=['GET'])
 def todas_peliculas():
+
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
 
     conP = sql_connection()
     curP = conP.cursor()
@@ -182,27 +271,38 @@ def todas_peliculas():
     #COnsulta de la tabla comentarios para calcular el promedio por pelicula
 
 
-    return render_template('todasPeliculas.html', page='peliculas', pelis = dataP)
+    return render_template('todasPeliculas.html', page='peliculas', pelis = dataP, user_tipo=user_tipo, user_id=user_id)
 
 
 
 @app.route('/agregarPelicula/', methods=['POST','GET'])
 def agregar_peliculas():
 
-    addPelis = add_pelicula()
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
 
-    if addPelis.validate_on_submit():
+    if user_tipo == "Admin":
 
-        con = sql_connection()
-        cur = con.cursor()
-        statement = 'INSERT INTO Peliculas (nombre, sinopsis, cartelera, imagen) VALUES (?,?,?,?)' 
-        cur.execute(statement, [addPelis.nombre.data, addPelis.sinopsis.data, addPelis.cartelera.data, addPelis.image.data])
-        con.commit()
+        addPelis = add_pelicula()
 
-        print("Se ingreso la película")
-        return redirect('/dashboard')
-    else: 
-        return render_template('agregarPelicula.html', form = addPelis)
+        if addPelis.validate_on_submit():
+
+            con = sql_connection()
+            cur = con.cursor()
+            statement = 'INSERT INTO Peliculas (nombre, sinopsis, cartelera, imagen) VALUES (?,?,?,?)' 
+            cur.execute(statement, [addPelis.nombre.data, addPelis.sinopsis.data, addPelis.cartelera.data, addPelis.image.data])
+            con.commit()
+
+            
+            return redirect('/dashboard')
+        else: 
+            return render_template('agregarPelicula.html', form = addPelis, user_tipo=user_tipo, user_id=user_id)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/dashboard/eliminar/<id_pelicula>')
@@ -218,31 +318,49 @@ def eliminar_pelicula(id_pelicula):
 def agregarComentario(id):
     
     
+    try:
+        user_tipo = session['user_tipo']
+        user_id = session['user_id']
+    except:
+        user_tipo = "Invitado"
+        user_id = 0
+
     conP = sql_connection()
     curP = conP.cursor()
     statementP = 'SELECT * FROM Peliculas WHERE id_pelicula = ?' 
     curP.execute(statementP, [id])
     dataP = curP.fetchall()
 
-    addCom = add_comentario()
-    if addCom.validate_on_submit():
-        
-        conA = sql_connection()
-        curA = conA.cursor()
-        statementA = 'INSERT INTO Comentarios (id_usuario, id_pelicula, texto, calificacion) VALUES (?,?,?,?)' 
-        curA.execute(statementA, [1,id,addCom.comentario.data, addCom.rating.data])
-        conA.commit()
+    if user_tipo != "Invitado":
 
-        return redirect('../detalleDePelicula/'+str(id))
-     
+        addCom = add_comentario()
+        if addCom.validate_on_submit():
+            
+            conA = sql_connection()
+            curA = conA.cursor()
+            statementA = 'INSERT INTO Comentarios (id_usuario, id_pelicula, texto, calificacion) VALUES (?,?,?,?)' 
+            curA.execute(statementA, [1,id,addCom.comentario.data, addCom.rating.data])
+            conA.commit()
+
+            return redirect('../detalleDePelicula/'+str(id))
+        
+        else:
+            
+            try:
+                return render_template('agregarComentario.html', form=addCom,page='agregarComentario', pelis = dataP[0], user_tipo=user_tipo, user_id=user_id)
+
+            except:
+                return redirect(url_for('index'))
     else:
-        
-        try:
-            return render_template('agregarComentario.html', form=addCom,page='agregarComentario', pelis = dataP[0])
+        return redirect(url_for('login'))
 
-        except:
-            return redirect(url_for('index'))
 
+@app.route("/logout", methods=['GET','POST'])
+def logout():
+    session.clear()
+    session['user_tipo'] = "Invitado"
+    session['user_id'] = 0
+    return redirect(url_for('index'))
     
 
 
